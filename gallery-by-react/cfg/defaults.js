26c80393153f79aa88d21b0fea802e459fa61a7e
sxy <834078042@qq.com>
2016-11-12 20:28:33 +0800

    gallery by  react

--- gallery-by-react/cfg/defaults.js
+++ gallery-by-react/cfg/defaults.js
@@ -49,6 +49,10 @@ function getDefaultModules() {
         loader: 'url-loader?limit=8192'
       },
       {
+        test: /\.json$/,
+        loader:'json-loader'
+      },
+      {
         test: /\.(mp4|ogg|svg)$/,
         loader: 'file-loader'
       }
--- gallery-by-react/src/components/Main.js
+++ gallery-by-react/src/components/Main.js
@@ -1,22 +1,45 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
-import React from 'react';
+import React from "react";
 
-let yeomanImage = require('../images/yeoman.png');
+//获取图片相关数据
+var imageDatas = require('../data/imageDatas.json');
+//利用自执行函数,讲图片名信息转成图片URL路径信息
+imageDatas = (function getImageUrl(imageDataArr) {
+  for (var i = 0; i < imageDataArr.length; i++) {
+    let singeImageData = imageDataArr[i];
+    singeImageData.imageUrl = require('../images/' + singeImageData.fileName);
+    imageDataArr[i] = singeImageData;
+  }
+  return imageDataArr;
+})(imageDatas);
 
+var ImgFigure = React.createClass({
+  render: function () {
+    return (
+      <figure>
+        <img/>
+      </figure>
+    )
+  }
+});
 class AppComponent extends React.Component {
   render() {
+
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <section className="stage">
+        <section className="img-sec">
+
+        </section>
+        <nav className="controller-nav">
+
+        </nav>
+      </section>
     );
   }
 }
 
-AppComponent.defaultProps = {
-};
+AppComponent.defaultProps = {};
 
 export default AppComponent;
